5ccdad39b5a8aafee78b435a248c0f3a99d42107
Victor Gil <victoredin94@hotmail.com>
2024-01-31 19:26:16 +0000

    Add Node.js support and notification test

---
+++ SchoolAPI.Test.WS/notification.spec.mjs
@@ -0,0 +1,51 @@
+import { expect, test } from 'vitest';
+import { createClient } from 'graphql-ws';
+import webSocketImpl from 'ws';
+
+const TEACHER_NAME = "Napoleon Bonaparte";
+const API = process.env.WEBAPP_URL || "localhost:5172/graphql";
+const query = "subscription { teacherAdded { name } }";
+const client = createClient({ url: `ws://${API}`, webSocketImpl });
+
+console.log("Connecting to:", API);
+
+function saveOrder() {
+  const headers = { 'Content-Type': 'application/json'};
+  const body = {
+    query: "mutation ($teacher: TeacherInput!) { saveTeacher(teacher: $teacher) { id } }",
+    variables: {
+      teacher: {
+        name: TEACHER_NAME
+      }
+    }
+  }
+
+  fetch(`http://${API}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify(body),
+  });
+}
+
+test("Should send notification after save an new order", () => {
+  return new Promise(async (resolve, reject) => {
+    client.subscribe(
+      { query },
+      {
+        next: data => {
+          console.log('data: ', data);
+          expect(data.data.teacherAdded.name).toBe(TEACHER_NAME);
+          resolve();
+        },
+        error: error => {
+          console.error('error: ', error);
+          reject();
+        }
+      },
+    );
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    saveOrder();
+  });
+}, 10000);
